571a8e6866f4ee4772c2880c6624ad9b0bb6eb22
Charlotte Knierim <cknierim@rcase-loanermbp.eur.adobe.com>
2016-03-15 10:26:51 +0100

    high-speed test done

--- feature_det.py
+++ feature_det.py
@@ -5,6 +5,11 @@ import matplotlib.cbook as cbook
 # threshold
 t = 10
 
+#circle_pos
+p = np.array([(0,3),(1,3),(2,2),(3,1),
+                  (3,0),(3,-1),(2,-2),(1,-3),
+                  (0,-3),(-1,-3),(-2,-2),(-3,-1),
+                  (-3,0),(-3,1),(-2,2),(-1,3)])
 def detect(path):
 	mage_file = cbook.get_sample_data(path)
 	image = plt.imread(image_file)
@@ -16,23 +21,21 @@ def detect(path):
 	#		 1 -> darker
 	#		 0 -> eq/ out of range
 	def circle_pix(x,y,i):
-		if (i-3) >= 0:
-			result[0] = image[i-3][j] - ip
-		if (i+3) < dim[0]
-			result[1] = image[i+3][j] - ip
-		if (j-3) >= 0:
-			result[2] = image[i][j-3] - ip
-		if (j+3) < dim[1]
-			result[3] = image[i][j+3] - ip
-		return t
+        (xd,yd) = p[i]
+        col_d = image[x+xd][y+yd] - image[x][y]
+        if  abs(col_d) < t:
+            return 0;
+        else:
+            result np.sign(col_d)
+    
+
 
 	def high_speed_test(i,j):
 		d = 0
 		b = 0
-		ip = image[i][j]
-		result  = np.array([])
+		result  = np.array([circle_pix(i,j,0),circle_pix(i,j,4)
+                            circle_pix(i,j,8),circle_pix(i,j,12)])
 		
-
 		for r in result :
 			if r == 1:
 				b+= 1
@@ -41,7 +44,7 @@ def detect(path):
 		return (b >= 3) or (d >= 3)	
 
 
-	for i in range(2,dim[0]-2):
+	for i in range(3,dim[0]-3):
 		for j in range(2,dim[1]-2):
 			if high_speed_test(i,j):
 				if coner_cand(i,j):
