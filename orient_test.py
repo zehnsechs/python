0d2c54fabb4fcc513d4aca3c3c68577f17505c52
Charlotte Knierim <cknierim@rcase-loanermbp.eur.adobe.com>
2016-03-24 17:17:50 +0100

    r

--- orient_test.py
+++ orient_test.py
@@ -1,7 +1,9 @@
 import cv2
 import feature_det as fd
+from matplotlib import pyplot as plt
+import numpy as np
 
-path = '/Users/cknierim/python/horse.png'
+path = '/Users/cknierim/python/3.png'
 img = cv2.imread(path)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -9,12 +11,12 @@ orb = cv2.ORB()
 orb.setInt('edgeThreshold',0)
 orb.setInt('patchSize', 7)
 orb.setInt('nLevels',1)
-orb.setInt('nFeatures',10)
+orb.setInt('nFeatures',20)
 kp = orb.detect(gray,None)
 list_kp = [(k.pt[1],k.pt[0],k.response,k.angle) for k in kp]
 
 my_dect = fd.Detector()
-my_points = my_dect.detect(gray,10,True,True,True,True)
+my_points = my_dect.detect(gray,20,True,True,True,True)
 
 
 # Initiate FAST object with default values
@@ -27,7 +29,24 @@ list_kpf = [(k.pt[1],k.pt[0]) for k in kpf]
 
 print "Total Keypoints ORB with nonmaxSuppression: ", len(kp),list_kp
 print'------------------------'
-print "Total Keypoints FAST with nonmaxSuppression: ", len(kpf),list_kpf
+#print "Total Keypoints FAST with nonmaxSuppression: ", len(kpf),list_kpf
 print '------------------------'
 print "Total Keypoints own impl with nonmaxSuppression: ", len(my_points) , my_points
 
+def get_point_list(l):
+    b = []
+    a = []
+    for (x,y,r,an) in l:
+        a.append(x)
+        b.append(y)
+    return a,b
+
+(la,lb) = get_point_list(my_points)
+plt.imshow(gray)
+plt.plot(lb,la,'ro')
+plt.savefig('my_fig_or.png')
+
+img3 = np.array(img) 
+cv2.drawKeypoints(img, kp,img3, color=(255,0,0))
+
+cv2.imwrite('fast_false_or.png',img3)
